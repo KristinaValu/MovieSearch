5c84732383dcdd908576fe80ee901a3cc16427eb
Kristina <93336791+KristinaValu@users.noreply.github.com>
2023-04-26 18:15:36 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchMovies from "./SearchMovies";
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
+      </div>
+    </>
+  );
+}
+
+export default App;
